3653a19571f9a71baadb72aa4ef2e9242e4694ad
sirmcclay <mcclay.design@gmail.com>
2021-02-22 12:31:01 -0300

    where with alias and calculations

--- filter/where.sql
+++ filter/where.sql
@@ -61,3 +61,8 @@ SELECT name,
   population / area AS population_density
 FROM cities
 WHERE population / area > 6000;
+
+SELECT name,
+  price * units_sold AS total_revenue
+FROM phones
+WHERE price * units_sold > 1000000;

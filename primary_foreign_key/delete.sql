2d2c7ec7c75deecfb301d433e190c345b9808e55
sirmcclay <mcclay.design@gmail.com>
2021-02-23 12:57:33 -0300

    describe all DELETE options

--- primary_foreign_key/delete.sql
+++ primary_foreign_key/delete.sql
@@ -6,3 +6,13 @@
 DELETE FROM users
 WHERE id = 1;
 
+-- ON DELETE NO ACTION
+  
+-- ON DELETE CASCADE
+  -- if try delete deletes all the data associated with
+
+-- ON DELETE SET NULL
+  -- if try delete then set all associated data foreign key to NULL
+
+-- ON DELETE SET DEFAULT
+  -- if try delete set associated foreign keys to a default value

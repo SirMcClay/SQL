6c867acc4059542082fc50eecc4b36e85513acc4
sirmcclay <mcclay.design@gmail.com>
2021-02-24 12:01:04 -0300

    another example

--- 04-relating_records_with_joins_and_aggregations/join.sql
+++ 04-relating_records_with_joins_and_aggregations/join.sql
@@ -3,3 +3,10 @@ SELECT contents,
   username
 FROM COMMENTS
   JOIN users ON users.id = comments.user_id;
+
+SELECT contents,
+  url
+FROM COMMENTS
+  JOIN photos ON photos.id = comments.photo_id;
+
+

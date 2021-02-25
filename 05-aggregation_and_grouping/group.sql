70434c93c100e481da36d3afb9adbd15858e218d
sirmcclay <mcclay.design@gmail.com>
2021-02-25 12:41:53 -0300

    using the group by clause

---
+++ 05-aggregation_and_grouping/group.sql
@@ -0,0 +1,6 @@
+-- Group by user_id all the rows and return the user id that appears on the rows
+  -- note that the rows are grouped with ids but are not accessible unless we
+  -- use a aggregate clause
+SELECT user_id
+FROM comments
+GROUP BY user_id;

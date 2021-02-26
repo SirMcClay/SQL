15e668ad72077352c23e51d73a7413e4912646b0
sirmcclay <mcclay.design@gmail.com>
2021-02-26 10:19:48 -0300

    now get the maximum id from all group ids

--- 05-aggregation_and_grouping/combine_group_and_aggregate.sql
+++ 05-aggregation_and_grouping/combine_group_and_aggregate.sql
@@ -2,3 +2,9 @@
 SELECT user_id
 FROM comments
 GROUP BY user_id;
+
+-- Just grouping all comments by user_id and show the maximun id value of all
+  -- rows grouped by id
+SELECT user_id, MAX(id)
+FROM comments
+GROUP BY user_id;

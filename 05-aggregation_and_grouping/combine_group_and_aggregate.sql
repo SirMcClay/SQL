8f20a83c1c8d6a33bd28c464266fd725fe7bb075
sirmcclay <mcclay.design@gmail.com>
2021-02-26 10:24:34 -0300

    group by id and count rows by id and rename the aggregate colunm

--- 05-aggregation_and_grouping/combine_group_and_aggregate.sql
+++ 05-aggregation_and_grouping/combine_group_and_aggregate.sql
@@ -8,3 +8,11 @@ GROUP BY user_id;
 SELECT user_id, MAX(id)
 FROM comments
 GROUP BY user_id;
+
+
+-- Just grouping all comments by user_id and count how many ids each user id has
+  -- this may be helpful to show us how many comments each user has been made
+  -- and we as well renamed count colunm to a more meaninfull name
+SELECT user_id, COUNT(id) AS num_comments_created
+FROM comments
+GROUP BY user_id;

8f92c326e9b18c908a468f3847120f685e6b5777
sirmcclay <mcclay.design@gmail.com>
2021-02-26 11:46:22 -0300

    group and count comments of all photos ids

--- 05-aggregation_and_grouping/combine_group_and_aggregate.sql
+++ 05-aggregation_and_grouping/combine_group_and_aggregate.sql
@@ -25,3 +25,8 @@ SELECT COUNT(*) FROM photos;
 SELECT user_id, COUNT(*) AS num_comments_created
 FROM comments
 GROUP BY user_id;
+
+-- Count all comments grouped for every photo id
+SELECT photo_id, COUNT(*)
+FROM comments
+GROUP BY photo_id;

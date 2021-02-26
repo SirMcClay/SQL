be7fb2a49420bea71a2647f5b33bb907ca2fee50
sirmcclay <mcclay.design@gmail.com>
2021-02-26 10:40:45 -0300

    using wildcard * for aggregate all rows

--- 05-aggregation_and_grouping/combine_group_and_aggregate.sql
+++ 05-aggregation_and_grouping/combine_group_and_aggregate.sql
@@ -9,10 +9,19 @@ SELECT user_id, MAX(id)
 FROM comments
 GROUP BY user_id;
 
-
 -- Just grouping all comments by user_id and count how many ids each user id has
   -- this may be helpful to show us how many comments each user has been made
   -- and we as well renamed count colunm to a more meaninfull name
 SELECT user_id, COUNT(id) AS num_comments_created
+FROM comments
+GROUP BY user_id;
+
+-- A GOTCHA - if we had a colunm with null values we must use * for aggregate
+  -- because this way we be able to count every rows
+SELECT COUNT(*) FROM photos;
+
+-- A refactor to a query above to use * wildcard to avoid ignore colunms with
+  -- null values
+SELECT user_id, COUNT(*) AS num_comments_created
 FROM comments
 GROUP BY user_id;

bddcd6ecf5ab5c32b499a15815dc095c1f32c561
sirmcclay <mcclay.design@gmail.com>
2021-02-27 13:19:04 -0300

    If we use UNION ALL the union dont remove duplicate rows, instead show all results

--- 07-unions_and_intersections/union.sql
+++ 07-unions_and_intersections/union.sql
@@ -13,3 +13,18 @@ UNION
   ORDER BY price / weight DESC
   LIMIT 4
 );
+
+-- If we use UNION ALL the union dont remove duplicate rows, instead show all results
+(
+  SELECT *
+  FROM products
+  ORDER BY price DESC
+  LIMIT 4
+)
+UNION ALL
+(
+  SELECT *
+  FROM products
+  ORDER BY price / weight DESC
+  LIMIT 4
+);

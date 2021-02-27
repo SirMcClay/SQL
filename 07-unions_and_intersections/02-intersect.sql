722ebedd8f8b5610dfe71947fa6822ef1761f06d
sirmcclay <mcclay.design@gmail.com>
2021-02-27 13:36:28 -0300

    Using INTERSECT ALL the resultset will contain the multiple results of all   -- querys if there are on both isolated

--- 07-unions_and_intersections/02-intersect.sql
+++ 07-unions_and_intersections/02-intersect.sql
@@ -13,3 +13,19 @@
    ORDER BY price / weight DESC
    LIMIT 4
  );
+
+ -- Using INTERSECT ALL the resultset will contain the multiple results of all
+  -- querys if there are on both isolated
+(
+  SELECT *
+  FROM products
+  ORDER BY price DESC
+  LIMIT 4
+ )
+ INTERSECT ALL
+ (
+   SELECT *
+   FROM products
+   ORDER BY price / weight DESC
+   LIMIT 4
+ );

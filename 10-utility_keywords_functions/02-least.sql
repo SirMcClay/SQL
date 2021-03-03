00c3a2cc62d1a2da9e217ae648f62c965e827bd3
sirmcclay <mcclay.design@gmail.com>
2021-03-03 10:30:24 -0300

    Here an example of LEAST operator where we show the price of product times 0.5 or 400 what is least

--- 10-utility_keywords_functions/02-least.sql
+++ 10-utility_keywords_functions/02-least.sql
@@ -1,2 +1,7 @@
 -- LEAST operator return the smaller value of values provided
 SELECT LEAST(1000, 20, 100);
+
+-- Here an example of LEAST operator where we show the price of product times
+  -- 0.5 or 400 what is least
+SELECT name, price, LEAST(price * 0.5, 400)
+FROM products;

4c715286d9fbf27b75146ffd85aa0d988ee51dae
sirmcclay <mcclay.design@gmail.com>
2021-03-03 09:57:25 -0300

    Here we use DISTINCT in conjunction with COUNT aggregator to quantify the number of unique departaments

--- 09-distinct/distinct.sql
+++ 09-distinct/distinct.sql
@@ -2,3 +2,8 @@
   -- and return as resultset all distinct values of a colunm
 SELECT DISTINCT department
 FROM products;
+
+-- Here we use DISTINCT in conjunction with COUNT aggregator to quantify the
+  -- number of unique departaments
+SELECT COUNT(DISTINCT department)
+FROM products;

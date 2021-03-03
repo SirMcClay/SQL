56719ddc217d1f006b00d56fed03dfccfe176142
sirmcclay <mcclay.design@gmail.com>
2021-03-03 10:11:43 -0300

    Here an example of use of GREATEST where we return on a resultset a cost of ship that is mininum 30 dollars or the 2 * weight, what is greater from both

--- 10-utility_keywords_functions/greatest.sql
+++ 10-utility_keywords_functions/greatest.sql
@@ -1,2 +1,7 @@
 -- GREATEST is a Postgresql operator to return the gratest value from a list
 SELECT GREATEST(200, 10, 30);
+
+-- Here an example of use of GREATEST where we return on a resultset a cost of
+  -- ship that is mininum 30 dollars or the 2 * weight, what is greater from both
+SELECT name, weight, GREATEST(30, 2 * weight)
+FROM products;
